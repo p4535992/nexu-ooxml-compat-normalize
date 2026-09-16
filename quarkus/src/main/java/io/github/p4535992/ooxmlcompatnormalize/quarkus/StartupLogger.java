7dc6138df1b859e2fd850bf11926088ca562fe76
4535992 <tentimarco0@gmail.com>
2026-09-16 11:45:00 +0200

    quarkus: log explicit startup marker

---
+++ quarkus/src/main/java/io/github/p4535992/ooxmlcompatnormalize/quarkus/StartupLogger.java
@@ -0,0 +1,15 @@
+package io.github.p4535992.ooxmlcompatnormalize.quarkus;
+
+import io.quarkus.runtime.StartupEvent;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.event.Observes;
+import org.jboss.logging.Logger;
+
+@ApplicationScoped
+public class StartupLogger {
+    private static final Logger LOG = Logger.getLogger(StartupLogger.class);
+
+    void onStart(@Observes StartupEvent event) {
+        LOG.info("OOXML Compat Normalize Quarkus local service started on 127.0.0.1:8080");
+    }
+}

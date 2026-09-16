0034a8c17bd5a50cd654974b53433ed2d2c5c1db
4535992 <tentimarco0@gmail.com>
2026-09-16 12:41:35 +0200

    quarkus: test local home page

--- quarkus/src/test/java/io/github/p4535992/ooxmlcompatnormalize/quarkus/NormalizeResourceTest.java
+++ quarkus/src/test/java/io/github/p4535992/ooxmlcompatnormalize/quarkus/NormalizeResourceTest.java
@@ -4,6 +4,7 @@ import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasItems;
 
@@ -17,5 +18,16 @@ class NormalizeResourceTest {
                 .statusCode(200)
                 .body("name", equalTo("ooxml-compat-normalize-quarkus"))
                 .body("formats", hasItems("docx", "xlsx", "pptx"));
+    }
+
+    @Test
+    void homePageIsAvailable() {
+        given()
+                .when().get("/")
+                .then()
+                .statusCode(200)
+                .contentType("text/html")
+                .body(containsString("OOXML Compat Normalize"))
+                .body(containsString("Normalizza e scarica"));
     }
 }

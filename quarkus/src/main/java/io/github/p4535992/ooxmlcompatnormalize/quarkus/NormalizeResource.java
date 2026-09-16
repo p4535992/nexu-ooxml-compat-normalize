cccc52ca37c096fdf50188cd808652df1632fbe8
4535992 <tentimarco0@gmail.com>
2026-09-16 12:54:41 +0200

    release: report rc7 from Quarkus API

--- quarkus/src/main/java/io/github/p4535992/ooxmlcompatnormalize/quarkus/NormalizeResource.java
+++ quarkus/src/main/java/io/github/p4535992/ooxmlcompatnormalize/quarkus/NormalizeResource.java
@@ -38,7 +38,7 @@ public class NormalizeResource {
     public InfoResponse info() {
         return new InfoResponse(
                 "ooxml-compat-normalize-quarkus",
-                "0.4.0-rc.6",
+                "0.4.0-rc.7",
                 List.of("docx", "xlsx", "pptx"),
                 List.copyOf(PROFILES)
         );

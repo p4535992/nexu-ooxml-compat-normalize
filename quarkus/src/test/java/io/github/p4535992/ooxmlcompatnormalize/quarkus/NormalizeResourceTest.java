833d166d222cefbe4f6a7c4f84f695ccd742f226
4535992 <tentimarco0@gmail.com>
2026-09-16 11:26:06 +0200

    quarkus: add local service smoke test

---
+++ quarkus/src/test/java/io/github/p4535992/ooxmlcompatnormalize/quarkus/NormalizeResourceTest.java
@@ -0,0 +1,21 @@
+package io.github.p4535992.ooxmlcompatnormalize.quarkus;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasItems;
+
+@QuarkusTest
+class NormalizeResourceTest {
+    @Test
+    void infoEndpointIsAvailable() {
+        given()
+                .when().get("/api/info")
+                .then()
+                .statusCode(200)
+                .body("name", equalTo("ooxml-compat-normalize-quarkus"))
+                .body("formats", hasItems("docx", "xlsx", "pptx"));
+    }
+}
